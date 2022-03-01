208b00d1d6cd1e4cb2fc0681db4d384589c2b29b
fyofio <muhammadisrofiordi@gmail.com>
2022-03-02 01:36:03 +0700

    fix product parser to accomodate discount

--- parsers/product.rb
+++ parsers/product.rb
@@ -2,8 +2,8 @@ html = Nokogiri.HTML(content)
 
 price = html.css('#prcIsum').text
 price =  html.css('#prcIsum_bidPrice').text if price.strip == ''
+price =  html.css('.vi-originalPrice').text if price.strip == ''
 raise 'something happend!' if price.strip == ''
-
 outputs << {
     _collection: "products",
     name: page['vars']['name'],
