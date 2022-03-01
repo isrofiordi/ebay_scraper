1da8a6e83d1909b4368d5c3d0c50bf3be62e732a
fyofio <muhammadisrofiordi@gmail.com>
2022-03-02 01:15:15 +0700

    fix product.rb parser to handle bid price

--- parsers/product.rb
+++ parsers/product.rb
@@ -1,6 +1,7 @@
 html = Nokogiri.HTML(content)
 
 price = html.css('#prcIsum').text
+price =  html.css('#prcIsum_bidPrice').text if price.strip == ''
 raise 'something happend!' if price.strip == ''
 
 outputs << {
